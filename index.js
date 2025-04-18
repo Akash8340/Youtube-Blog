22e92b03013917b0be37c05e38379948bc5a3af3
Akash Singh <akashsingh854469@gmail.com>
2025-04-18 20:31:37 +0530

    first comment after creation

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const path = require("path");
+const mongoose= require("mongoose");
+const cookieParser=require("cookie-parser");
+
+const Blog = require("./models/blogs");
+
+const userRoute=require("./routes/user");
+const blogRoute=require("./routes/blog");
+
+const { checkForAuthenticationCookies } = require("./middlewares/authentication");
+
+const app=express();
+const PORT=8000;
+
+mongoose.connect("mongodb://localhost:27017/blogify").then(e=>console.log("MongoDB connected"));
+
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended:false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookies("token"));
+app.use(express.static(path.resolve("./public/images")));
+
+app.get("/",async(req,res)=>{
+    const allBlogs = await Blog.find({});
+    res.render('home',{
+        user:req.user,
+        blogs:allBlogs,
+    });
+})
+
+app.use("/users",userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT,()=>console.log("Server Started at Port:",PORT));
---
+++ models/user.js
@@ -0,0 +1,53 @@
+const {createHmac,randomBytes} = require('crypto');
+const {createTokenForUser}=require('../services/authentication');
+const mongoose= require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    fullName:{ type:String, required:true},
+    email:{ type:String, required:true, unique:true},
+    salt:{ type: String,},
+    password:{ type:String, required:true},
+    profileImageURL:{type: String, default: "/default.png" },
+    role:{ type: String ,enum: ["USER","ADMIN"], default:"USER"},
+},{timestamps: true}
+);
+
+userSchema.pre("save", function(next){
+    const user=this;
+
+    if(!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString('hex');
+    const hashedPassword = createHmac("sha256",salt)
+    .update(user.password)
+    .digest("hex");
+
+    this.salt = salt;
+    this.password=hashedPassword;
+
+    next();
+});
+
+//virtual function
+userSchema.static('matchPasswordAndGenerateToken', async function(email,password){
+    const user= await this.findOne({email});
+
+    if(!user) throw new Error("user not found checked");
+
+    const salt=user.salt;
+    const hashedPassword=user.password;
+   
+    const userProvidedHash = createHmac("sha256",salt)
+        .update(password)
+        .digest("hex");
+
+    if(hashedPassword !== userProvidedHash) throw new Error("Invalid Password");
+
+    const token = createTokenForUser(user);
+    return token;
+
+})
+
+const User=mongoose.model('user',userSchema);
+
+module.exports = User;
